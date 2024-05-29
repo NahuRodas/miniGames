8032561d2c8ac73d90858b6abd6acbb8b100140a
Nahuel Rodas <cesarnahuelrodas@gmail.com>
2024-05-29 16:55:28 -0300

    Guess the number game

--- src/games/guessNumber/guessNumber.js
+++ src/games/guessNumber/guessNumber.js
@@ -1,21 +1,43 @@
 const min = 1, max = 100;
-let userGuess = 0, number = 0, button = document.getElementById("button"), screen = document.getElementById("screen");
+let     userGuess = 0, number = 0, 
+        guessButton = document.getElementById("guess"), 
+        screen = document.getElementById("screen"),
+        newButton = document.getElementById("new"),
+        userInput = document.getElementById("user"),
+        curentGuess = document.getElementById("currentGuess");
+
 number = Math.floor(Math.random() * 100 + 1);
 
-
-button.onclick = function() {
-    userGuess = Number(document.getElementById("user").value);
+function submit() {
+    userGuess = Number(userInput.value);
+    curentGuess.textContent = userGuess;
 
     if ( isNaN(userGuess) || userGuess < 1 || userGuess > 100 ) {
-        screen.textContent = "You must enter a Number in between 1 and 100";
+        screen.textContent = "You must enter a Number in between 1 and 100.";
+        curentGuess.textContent = "Bruh";
     }else if (userGuess < number) {
         screen.textContent = "The Number is higher.";
     }else if (userGuess > number) {
         screen.textContent = "The Number is lower.";
     }else{
-        screen.textContent = "Correct! The number is " + number;
+        screen.textContent = "Correct! The number is " + number + "!";
     }
 
-    console.log(number);
-    console.log(userGuess);
+    userInput.value = "";
+}
+
+userInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+       submit();
+    }
+  });
+
+guessButton.onclick = function() {
+    submit();
+}
+
+newButton.onclick = function(){
+    number = Math.floor(Math.random() * 100 + 1);
+    curentGuess.textContent = "";
+    screen.textContent = "Enter any number (1 to 100) to start!";
 }
